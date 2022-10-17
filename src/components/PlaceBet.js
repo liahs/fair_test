3405ac5aeed634c5c428ddaf1d08c28c5cc9af1d
kapilDhawan <kapild@solidappmaker.com>
2022-10-17 23:19:37 +0530

    number input field

--- src/components/PlaceBet.js
+++ src/components/PlaceBet.js
@@ -1,7 +1,8 @@
-import { Modal, TextField, Typography } from "@mui/material";
+import { Input, Modal, TextField, Typography } from "@mui/material";
 import { Box } from "@mui/system";
-import { CANCEL } from "../assets";
-
+import { useState } from "react";
+import { ArrowDown, CANCEL } from "../assets";
+import '../components/index.css'
 const PlaceBet = ({ open, handleClose }) => {
 
     const SetBet = () => {
@@ -18,27 +19,49 @@ const PlaceBet = ({ open, handleClose }) => {
         )
     }
 
+    const NumberInput = () => {
+        const [value, setValue] = useState('90.00')
+        return (
+            <Box sx={{ postion: 'relative' }}>
+
+                <TextField variant="standard"
+                    value={value}
+                    InputProps={{
+                        disableUnderline: true,
+                        style: { fontSize: "11px", fontWeight: "600", textAlign: 'center', marginLeft: 5 },
+                        sx: {
+                            fontSize: { laptop: "10px", mobile: "7px", textAlign: 'center' }
+                        }
+                    }}
+                    onChange={(i) => {
+                        setValue(i.target.value)
+                    }}
+                    sx={{ borderColor: "white", display: "flex", flex: 1, marginLeft: "5px", fontSize: { laptop: "10px", mobile: "8px" } }} />
+                <Box sx={{ position: 'absolute', marginTop: -3.5, marginLeft: 7.5, height: '30px', justifyContent: 'space-evenly', display: 'flex', flexDirection: 'column' }}>
+                    <Box
+                        onClick={() => {
+                            setValue((i) => (parseInt(i) + 1).toFixed(2))
+                        }}
+                        sx={{ width: '14px', height: '14px', background: '#004A25', justifyContent: 'center', alignItems: 'center', display: 'flex' }} ><img src={ArrowDown} style={{ width: '7px', height: '4px', transform: 'rotate(180deg)' }} ></img></Box>
+                    <Box
+                        onClick={() => {
+                            setValue((i) => (parseInt(i) - 1).toFixed(2))
+                        }}
+                        sx={{ width: '14px', height: '14px', background: '#004A25', justifyContent: 'center', alignItems: 'center', display: 'flex' }} ><img src={ArrowDown} style={{ width: '7px', height: '4px' }} ></img></Box>
+
+                </Box>
+            </Box >
+
+        )
+    }
     const RowComponent = ({ data, containerStyle, header }) => {
         return (
             <Box sx={[{ height: "20px", backgroundColor: header ? "#319E5B" : "#FFFFFF", height: header ? "20px" : "30px", flexDirection: "row", display: "flex" }, containerStyle]}>
-                {data?.map((x, i) => <Box sx={{ flex: i == 0 ? 6 : 3, px: i == 0 ? "10px" : "0px", marginLeft: i != 0 ? "2px" : 0, display: "flex", alignItems: "center", background: i != 0 ? (header ? "black" : "#F1F1F1") : "transparent", justifyContent: i != 0 ? "center" : "flex-start" }}>
+                {data?.map((x, i) => <Box sx={{ flex: i == 0 ? 6 : (i == 1 || !header) ? 4.5 : header ? 4.5 : 3, px: i == 0 ? "10px" : "0px", marginLeft: i != 0 ? "2px" : 0, display: "flex", alignItems: "center", background: i != 0 ? (header ? "black" : "#F1F1F1") : "transparent", justifyContent: i != 0 ? "center" : "flex-start" }}>
                     {!header && i == 0 && <img style={{ width: '15px', height: '15px', marginRight: '5px' }} src={CANCEL} />}
-                    {(i !== 1 || header) && < Typography sx={{ fontSize: { laptop: header ? "10px" : "12px", mobile: header ? "8px" : "10px" }, fontWeight: header ? "normal" : "600", color: header ? "text.white" : "text" }}>{x}</Typography>}
+                    {(i !== 1 || header) && < Typography sx={{ fontSize: { laptop: header ? "10px" : "11px", mobile: header ? "8px" : "10px" }, fontWeight: header ? "normal" : "600", color: header ? "text.white" : "text" }}>{x}</Typography>}
                     {(i == 1 && !header) &&
-                        <TextField
-
-                            inputProps={{
-                                disableUnderline: true,
-                                style: {
-                                    fontSize: '10px',
-                                    fontWeight: "600",
-                                    height: '100%',
-
-                                },
-
-                            }}
-                            fo
-                            type={'number'} defaultValue={x} variant="standard" sx={{ height: '30px', fontSize: '1px' }} />
+                        <NumberInput />
                     }
 
                 </Box>)
@@ -58,7 +81,7 @@ const PlaceBet = ({ open, handleClose }) => {
             onClose={handleClose}
         >
             <Box sx={{ width: '100vw', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', background: 'rgba(0,0,0,.1)', alignSelf: 'center' }} >
-                <Box sx={{ width: '300px', background: 'white', }} >
+                <Box sx={{ width: '350px', background: 'white', }} >
                     <SetBet />
                     <Box sx={{ display: 'flex', flex: 1, paddingY: '2vh', justifyContent: 'space-evenly' }}>
                         <CustomButton onClick={handleClose} title={'Reset'} color={'red'} />
