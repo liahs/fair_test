0bc2cff7aa1046cc06a1c7b6955491371ab3d88c
himmat <himats514@gmail.com>
2022-10-30 19:31:20 +0530

    workdone

---
+++ src/components/AllRateOdds.js
@@ -0,0 +1,109 @@
+import { Box, Typography } from "@mui/material"
+import { MyBet } from "../assets"
+import StyledImage from './StyledImage'
+const data = [
+    {
+        title: "BOOKMAKER",
+        time: "03:23 AM",
+        type: "Back",
+        odds: "90.00",
+        stake: "1000.00",
+        country: 'INDIA'
+    },
+    {
+        title: "Match odds",
+        time: "03:23 AM",
+        type: "Lay",
+        odds: "90.00",
+        stake: "1000.00",
+        country: 'INDIA'
+    },
+
+]
+const AllRateOdds = () => {
+    return (
+        <Box sx={{ width: { mobile: "96%", laptop: '100%' }, marginY: { mobile: '.7vh', laptop: '1vh' }, padding: .2, background: 'white' }}>
+            <Box sx={[{ width: '100%', height: "42px", justifyContent: 'space-between', alignItems: 'center', paddingLeft: '10px', paddingRight: '4px', marginBottom: '.1vh', display: 'flex', }, (theme) => ({
+                backgroundImage: `${theme.palette.primary.headerGradient}`
+            })]} >
+                <Typography sx={{ fontWeight: '12px', color: 'white', fontWeight: '700' }} >All Rate Odds</Typography>
+
+                <Box sx={{ width: '100px', height: '90%', background: 'white', justifyContent: 'center', borderRadius: '3px', alignItems: 'center', display: 'flex', flexDirection: 'column' }}>
+                    <Typography sx={{ fontSize: '12px', fontWeight: '700', color: '#FF1111' }} >Total Bet</Typography>
+                    <Typography sx={{ fontSize: '12px', fontWeight: '700', color: "#0B4F26" }} >999</Typography>
+
+                </Box>
+            </Box>
+            <RowComponent header={true} data={["Market", "Favourite", "Back/Lay", "Odds", "Stake"]} />
+
+            {
+                [...data, ...data, ...data, ...data].map((i, k) => {
+                    return (
+                        <RowComponent header={false} data={i} />
+                    )
+                })
+            }
+        </Box>
+    )
+}
+
+const RowComponent = ({ header, data }) => {
+    const getColor = () => {
+
+        if (header) {
+            return "black"
+        }
+        else if (data?.type == "Back") {
+            return "#B3E0FF"
+        }
+        else {
+            return "#FF9292"
+        }
+
+    }
+    return (
+        <Box sx={{ width: '100%', height: header ? '20px' : '42px', background: 'white', justifyContent: 'space-between', alignItems: 'center', display: 'flex' }}>
+
+
+            {!header && <>
+                <SingleBox color={getColor} data={data.title} first={true} header={header} />
+                <SingleBox color={getColor()} data={data} up={true} header={header} />
+                <SingleBox color={getColor()} data={data?.type} header={header} />
+                <SingleBox color={getColor()} data={data?.odds} header={header} />
+                <SingleBox color={getColor()} data={data?.stake} header={header} /></>}
+            {header && <>
+                <SingleBox color={getColor} data={data[0]} header={header} />
+                <SingleBox color={getColor()} data={data[1]} header={header} />
+                <SingleBox color={getColor()} data={data[2]} header={header} />
+                <SingleBox color={getColor()} data={data[3]} header={header} />
+                <SingleBox color={getColor()} data={data[4]} header={header} /></>}
+
+
+        </Box>
+    )
+}
+const SingleBox = ({ data, header, color, up, first }) => {
+
+    return !header ? first ? (
+        <Box sx={{ width: '100%', height: '40px', background: "#F1C550", marginX: { mobile: '1px', laptop: '0.4px' }, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <Typography sx={{ fontWeight: '700', fontSize: '10px', color: 'black' }} >{data}</Typography>
+        </Box>
+    ) : up ? (
+        <Box sx={{ width: '100%', height: '40px', flexDirection: 'column', background: color, marginX: { mobile: '1px', laptop: '0.4px' }, display: 'flex', justifyContent: 'center' }}>
+            <Typography sx={{ fontWeight: '600', fontSize: '8px', color: 'black', textAlign: 'end', marginRight: '3px' }} >{data.time}</Typography>
+            <Box sx={{ height: '.4vh' }} ></Box>
+            <Typography sx={{ fontWeight: '600', fontSize: '12px', color: 'black', textAlign: 'start', marginLeft: '3px' }} >{data.country}</Typography>
+
+        </Box>
+    ) : (
+        <Box sx={{ width: '100%', height: '40px', background: color, marginX: { mobile: '1px', laptop: '0.4px' }, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <Typography sx={{ fontWeight: '600', fontSize: '12px', color: 'black' }} >{data}</Typography>
+        </Box>
+    ) : header &&
+    (
+        <Box sx={{ width: '100%', height: '20px', background: 'black', marginX: { mobile: '1px', laptop: '0.4px' }, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <Typography sx={{ fontWeight: '400', fontSize: '12px', color: 'white' }} >{data}</Typography>
+        </Box>
+    )
+}
+export default AllRateOdds;
--- src/components/MathComponent.js
+++ src/components/MathComponent.js
@@ -1,14 +1,14 @@
 // https://www.sciencekids.co.nz/images/pictures/flags680/India.jpg
 // https://www.sciencekids.co.nz/images/pictures/flags680/Pakistan.jpg
-import { TextField, Typography, useTheme, useMediaQuery } from "@mui/material"
+import { TextField, Typography, useTheme, useMediaQuery, Divider } from "@mui/material"
 import { Box } from "@mui/system"
 
 const MatchComponent = ({ }) => {
 
     return (
-        <Box sx={[{ width: { tablet: "55%", mobile: "97%", laptop: '100%' }, display: 'flex', flexDirection: 'column', marginX: { laptop: '0vw', mobile: '0px', tablet: '0px' }, marginY: "2vh", padding: '2px', borderRadius: '2px', background: 'white', alignSelf: { mobile: 'center', tablet: 'center', laptop: "flex-start" } }]}>
+        <Box sx={[{ width: { tablet: "55%", mobile: "96%", laptop: '100%' }, display: 'flex', flexDirection: 'column', marginX: { laptop: '0vw', mobile: '0px', tablet: '0px' }, marginY: { laptop: "1vh", mobile: '.7vh' }, padding: '2px', borderRadius: '2px', background: 'white', alignSelf: { mobile: 'center', tablet: 'center', laptop: "flex-start" } }]}>
             <Typography sx={{ fontSize: { mobile: '9px', tablet: '10px', laptop: '12px' }, marginLeft: '1vw', flexDirection: 'row' }} >World T20 <span style={{ color: 'grey' }} >Sunday 23 OCT, 1:30 pm</span></Typography>
-            <Box sx={{ display: 'flex', paddingBottom: '1vh', flexDirection: 'column', flex: 1, justifyContent: 'flex-end', borderBottom: '.4px solid gray' }} >
+            <Box sx={{ display: 'flex', paddingBottom: '1vh', flexDirection: 'column', flex: 1, justifyContent: 'flex-end', borderBottom: '0px solid', borderColor: 'gray' }} >
                 <Box sx={{ display: 'flex' }} >
                     <Box sx={{ display: 'flex', flex: 1, alignItems: 'center', justifyContent: 'flex-end', flexDirection: 'column' }} >
                         <img style={{ width: '50px', height: '35px', "boxShadow": "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)" }} src="https://www.sciencekids.co.nz/images/pictures/flags680/India.jpg" />
@@ -26,6 +26,9 @@ const MatchComponent = ({ }) => {
                 <Typography sx={{ fontSize: { mobile: '8px', tablet: '8px', laptop: '10px' }, alignSelf: 'center', marginTop: '1vh', color: 'gray', fontWeight: '600' }}  >Super 12 .T20 16 of 45</Typography>
 
             </Box>
+            <Box sx={{ width: '100%', background: 'rgba(211,211,211)', height: '1px' }} ></Box>
+
+
             <Typography sx={{ textAlign: 'center', fontWeight: '700', fontSize: '10px', paddingY: '1vh' }} >WIN PROBABILITY</Typography>
 
         </Box >
