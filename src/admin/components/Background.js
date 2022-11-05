c3c9d2d9484fc1988cd42fb1c62ada17c0061f8f
liahs <liahschhetri2@gmail.com>
2022-11-05 19:56:01 +0530

    added minHeight for bg instead fix

--- src/admin/components/Background.js
+++ src/admin/components/Background.js
@@ -2,7 +2,7 @@ import { Box } from "@mui/material";
 import Back from '../assets/back.png'
 export default function Background({ children }) {
     return (
-        <Box sx={{ height:"100vh",width:"100vw", backgroundImage: `url(${Back})`, backgroundRepeat: "no-repeat", backgroundSize: "100vw 100vh" }}>
+        <Box sx={{ minHeight:"100vh",width:"100vw", backgroundImage: `url(${Back})`, backgroundRepeat: "no-repeat", backgroundSize: "100vw 100vh" }}>
             {children}
         </Box>
     )
